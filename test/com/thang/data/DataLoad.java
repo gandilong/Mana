a785c155e736d50dc319060521b8e2f5be306b20
gandilong <gandilong@yeah.net>
2013-06-19 10:17:19 +0800

    add test,config,.settings,.classpath,.project file

---
+++ test/com/thang/data/DataLoad.java
@@ -0,0 +1,11 @@
+package com.thang.data;
+
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class DataLoad {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ test/com/thang/test/ExecTest.java
@@ -0,0 +1,24 @@
+package com.thang.test;
+
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+
+public class ExecTest {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws ExecuteException 
+	 */
+	public static void main(String[] args) throws ExecuteException, IOException {
+		// TODO Auto-generated method stub
+
+		CommandLine cmdLine = CommandLine.parse("java -version");
+		DefaultExecutor executor = new DefaultExecutor();
+		int exitValue = executor.execute(cmdLine);
+	}
+
+}
